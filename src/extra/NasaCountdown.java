46be93220a65eac0f758a482d603a6b344f09d94
Tacoalarmclock <league@iMac-2.attlocal.net>
2019-02-25 18:27:36 -0800

    Finished something and started rock paper scissors

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -17,27 +17,29 @@ import javax.swing.JOptionPane;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-;
+		String Z = JOptionPane.showInputDialog("Where start counting");
 		// 3. Change the countdown to use the new starting point
-int countdown = Integer.parseInt(null)
 
-a = JOptionPane.showInputDialog("Where start counting");
 		// 1. Print a countdown from 10 to 0 on the console
-	System.out.println("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0");
-			// 4. Use the speak method to hear the countdown.
-		speak("supercalifragilisticxpialidocios");
-			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
+		System.out.println("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0");
+		int o = Integer.parseInt(Z);
+		// 4. Use the speak method to hear the countdown.
+		for (int i = o; i > 0; i--) {
+			String p = Integer.toString(i);
+			speak(p);
+
+			// speak("supercalifragilisticxpialidocios");
+			Thread.sleep(1000);
+		}
 		// 5. when the counting is done, speak "blastoff!"
+		speak("blastoff");
 	}
 
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
---
+++ src/random/ComplicatedRPC.java
@@ -0,0 +1,24 @@
+package random;
+
+import java.util.Random;
+
+public class ComplicatedRPC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+int h = 8;
+int i = 9;
+for (int y=0;y<11;y++) {
+		int z = new Random().nextInt(10);
+		
+		System.out.println(z);
+	}
+	}
+}
