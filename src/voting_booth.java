4245a44ebb076991ca01959189aba6291fc0cfe8
Tacoalarmclock <league@iMac-2.attlocal.net>
2019-03-11 18:02:44 -0700

    yay

---
+++ src/voting_booth.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class voting_booth {
+
+	public static void main(String[] args) {
+int k = Integer.parseInt( JOptionPane.showInputDialog("how old are you"));
+if (k<18) {
+	JOptionPane.showMessageDialog(null, "nobody cares what you think xD");
+}
+else {
+	JOptionPane.showInputDialog("who should be the president?");
+}
+	
+	}
+
+}
